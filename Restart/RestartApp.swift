b74a4129e124a25e7d5d277203b1088c63007692
umut9191 <umutsurmeli91@gmail.com>
2022-02-22 22:39:38 +0300

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Macintosh on 22.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
