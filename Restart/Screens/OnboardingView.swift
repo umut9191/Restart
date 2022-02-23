cf409eff0e5bb8db984653024b60018c4ad788cb
umut9191 <umutsurmeli91@gmail.com>
2022-02-24 01:40:15 +0300

    Custom button code with gesture added ,Center image and circle code added

--- Restart/Screens/OnboardingView.swift
+++ Restart/Screens/OnboardingView.swift
@@ -13,10 +13,11 @@ struct OnboardingView: View {
         ZStack {
             Color("ColorBlue")
                 .ignoresSafeArea(.all,edges: .all)
+            
             VStack(spacing:20) {
-                
                 //MARK: - Header
                 Spacer()
+                
                 VStack(spacing:0){
                     Text("Share.")
                         .font(.system(size: 60))
@@ -34,21 +35,86 @@ struct OnboardingView: View {
                 }//Header
                 //MARK: - Center
                 
-                
+                ZStack  {
+                    ZStack{
+                        Circle()
+                            .stroke(.white.opacity(0.2),lineWidth: 40)
+                            .frame(width: 260, height: 260, alignment: .center)
+                        Circle()
+                            .stroke(.white.opacity(0.2),lineWidth: 80)
+                            .frame(width: 260, height: 260, alignment: .center)
+                    }//: Zstack
+                    Image("character-1")
+                        .resizable()
+                        .scaledToFit()
+                }//: Center
+                Spacer()
                 //MARK: - Footer
+               
+                ZStack{
+                    
+
+                    //Part Of the custom Button
+                    
+                    //1.BackGround (Static)
+                    
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                    Capsule()
+                        .fill(Color.white.opacity(0.2))
+                        .padding(8)
+                    //2.Call -To - Action (Static)
+                    
+                    Text("Get Started")
+                        .font(.system(.title3,design: .rounded))
+                        .fontWeight(.bold)
+                        .foregroundColor(.white)
+                        .offset(x:20)
+                    
+                    //3.Capsule (Dynamic width)
+                    HStack{
+                        Capsule()
+                            .fill(Color("ColorRed"))
+                            .frame(width:80)
+                        Spacer()
+                    }
+                    //4.Circle (Draggable)
+                    HStack {
+                        ZStack{
+                            Circle()
+                                .fill(Color("ColorRed"))
+                            Circle()
+                                .fill(.black.opacity(0.15))
+                                .padding(8)
+                            Image(systemName: "chevron.right.2")
+                                .font(.system(size: 24,weight:.bold))
+                            
+                        }
+                        .foregroundColor(.white)
+                    .frame(width: 80, height: 80, alignment: .center)
+                    .onTapGesture {
+                        isOnboarding = false
+                    }
+                        Spacer()
+                    }//HStack
+                    
+                    
+                    
+                    
+                }//:Footer
+                .frame(height: 80, alignment: .center)
+                .padding()
                 
                 
-                
-                
-                
-//                Text("Onboarding")
-//                    .font(.largeTitle)
-//
-//                Button( action:{
-//                    isOnboarding = false
-//                })
-//                    { Text("Start")}
+                //                Text("Onboarding")
+                //                    .font(.largeTitle)
+                //
+                //                Button( action:{
+                //                    isOnboarding = false
+                //                })
+                //                    { Text("Start")}
             }//VStack
+            
         }//ZStack
     }
 }
